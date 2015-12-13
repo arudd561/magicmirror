28f35c0d53a06ae0bd8f0a3b3cc544073d527f35
root <root@raspberrypi.(none)>
2015-12-13 19:55:16 +0000

    first commit

---
+++ js/config.js
@@ -0,0 +1,42 @@
+var config = {
+    lang: 'en',
+    time: {
+        timeFormat: 12
+    },
+    weather: {
+        //change weather params here:
+        //units: metric or imperial
+        params: {
+            q: 'Sandy,Utah,USA',
+            units: 'imperial',
+            // if you want a different lang for the weather that what is set above, change it here
+            lang: 'en',
+            APPID: 'YOUR_FREE_OPENWEATHER_API_KEY'
+        }
+    },
+    compliments: {
+        interval: 30000,
+        fadeInterval: 4000,
+        morning: [
+            'Good morning, handsome!',
+            'Enjoy your day!',
+            'How was your sleep?'
+        ],
+        afternoon: [
+            'Hello, beauty!',
+            'You look sexy!',
+            'Looking good today!'
+        ],
+        evening: [
+            'Wow, you look hot!',
+            'You look nice!',
+            'Hi, sexy!'
+        ]
+    },
+    calendar: {
+        maximumEntries: 10
+    },
+    news: {
+        feed: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+    }
+}
